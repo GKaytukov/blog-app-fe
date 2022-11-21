6d450075747414a35e04a7fbff21759b61088af2
Gerly Kaytukov <gerlyecc@yahoo.com>
2022-11-21 16:13:34 -0500

    first commit

---
+++ src/pages/AddPost.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+
+export default function AddPost () {
+    const [form, setForm] = useState({})
+
+    const submitPost = e => {
+        e.preventDefault()
+
+        fetch('http://localhost:4040', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json', 
+            }, 
+            body: JSON.stringify(form), 
+        })
+    }
+
+    const handleForm = e => {
+        setForm({...form, [e.target.name]: e.target.value})
+
+    }
+
+    console.log(form)
+
+    return (
+        <div className='container'>
+            <h1>Add Post</h1>
+            <form action='submit'>
+                <label htmlFor='' >Author</label>
+                <input name='text' onChange={handleForm} />
+                <br />
+                <label htmlFor='' >Text</label>
+                <input name='text' onChange={handleForm} />
+                <br />
+                <label htmlFor='' >Date</label>
+                <input name='date' onChange={handleForm} />
+                <br />
+                <button type='submit' onClick={submitPost}>
+                    Add new post
+                </button>
+            </form>
+        </div>
+    )
+}
